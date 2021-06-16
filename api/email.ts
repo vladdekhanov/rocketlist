91cf9a8af14c6c8a55edd2270eccdc42108bff73
vladdekhanov <vladdexx@yandex.ru>
2021-06-16 09:44:27 +0300

    test email send

---
+++ api/email.ts
@@ -0,0 +1,37 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import nodemailer from 'nodemailer';
+
+export default async(request: VercelRequest, response: VercelResponse) => {
+    const { name = 'World' } = request.query
+
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    response.status(200).send(`Email sent!`)
+}
