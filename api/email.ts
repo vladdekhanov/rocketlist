bac9c2ae59e66ab99795f2c7ba83d114296fc6e9
vladdekhanov <vladdexx@yandex.ru>
2021-06-16 09:45:27 +0300

    test email send to me

--- api/email.ts
+++ api/email.ts
@@ -22,7 +22,7 @@ export default async(request: VercelRequest, response: VercelResponse) => {
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
+        to: "vladdexx@yandex.ru", // list of receivers
         subject: "Hello ✔", // Subject line
         text: "Hello world?", // plain text body
         html: "<b>Hello world?</b>", // html body
